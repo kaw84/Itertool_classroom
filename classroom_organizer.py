0f69231ed9cb7deebb20393768111db744b84ff7
kaw84 <128994708+kaw84@users.noreply.github.com>
2023-07-09 17:11:07 +0100

    Update classroom_organizer.py

--- classroom_organizer.py
+++ classroom_organizer.py
@@ -1,26 +1,30 @@
 from roster import student_roster
 import itertools
-
-
 # Import modules above this line
 class ClassroomOrganizer:
-    def __init__(self):
-        self.sorted_names = self._sort_alphabetically(student_roster)
+  def __init__(self):
+    self.sorted_names = self._sort_alphabetically(student_roster)
+    self.index = 0
 
-    def _sort_alphabetically(self, students):
-        names = []
-        for student_info in students:
-            name = student_info['name']
-            names.append(name)
-        return sorted(names)
+  def __next__(self):
+    self.index += 1
+    return self.sorted_names[self.index - 1]
 
-    def sitting(self, students):
-        child_combos = list(itertools.combinations(students, 2))
-        return child_combos
+  def _sort_alphabetically(self,students):
+    names = []
+    for student_info in students:
+      name = student_info['name']
+      names.append(name)
+    return sorted(names)
 
-    def get_students_with_subject(self, subject):
-        selected_students = []
-        for student in student_roster:
-            if student['favorite_subject'] == subject:
-                selected_students.append((student['name'], subject))
-        return selected_students
+  def sitting(self, students):
+    child_combos = list(itertools.combinations(students, 2))
+    return child_combos
+    
+
+  def get_students_with_subject(self, subject):
+    selected_students = []
+    for student in student_roster:
+      if student['favorite_subject'] == subject:
+        selected_students.append((student['name'], subject))
+    return selected_students
